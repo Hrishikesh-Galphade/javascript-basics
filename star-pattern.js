a00d629deff4ccc88c5131dede0e324630fa8122
Hrishikesh Galphade <hrishikesh.galphade@gmail.com>
2024-07-03 07:35:36 +0530

    updated for loop

---
+++ star-pattern.js
@@ -0,0 +1,11 @@
+var row=10;
+var column=15;
+
+for(var i=1;i<= row;i++)
+    {
+        var str="";
+        for(var j=1; j<= column;j++){
+            str=str+"*";
+        }
+        console.log(str);
+    }
