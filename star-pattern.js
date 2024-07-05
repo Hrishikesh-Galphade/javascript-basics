965aff3bc6a0ec149d18732af47ce8eaee500d4d
Hrishikesh Galphade <hrishikesh.galphade@gmail.com>
2024-07-05 07:32:49 +0530

    updated for new pattern

--- star-pattern.js
+++ star-pattern.js
@@ -1,11 +1,34 @@
-var row=10;
-var column=15;
+// var row=10;
+// var column=15;
+
+// for(var i=1;i<= row;i++)
+//     {
+//         var str="";
+//         for(var j=1; j<= column;j++){
+//             str=str+"*";
+//         }
+//         console.log(str);
+//     }
+
+var row=4;
+var column=4;
 
 for(var i=1;i<= row;i++)
     {
         var str="";
         for(var j=1; j<= column;j++){
-            str=str+"*";
+            
+            if(j<= row-i)
+            {
+                str=str+" ";
+            }
+            else{
+                str=str+"*";
+            }
+            
         }
         console.log(str);
     }
+
+
+    
